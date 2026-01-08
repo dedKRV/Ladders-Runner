b115378b3a381d5f4f503748a65bdcc69cf20d16
dedKRV <arsenij022018@gmail.com>
2026-01-08 13:51:40 +0300

    КороткоеТнекстуры

---
+++ assets/tilesets/set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="set" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="361" columns="19">
+ <image source="monochrome_tilemap.png" width="339" height="339"/>
+</tileset>
